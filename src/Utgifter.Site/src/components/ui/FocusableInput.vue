ffe087548ca198adde86228ca58e63d787d3cb15
lucas.dellisola <lucas.dellisola@rystadenergy.com>
2025-12-26 09:51:54 +0100

    Fixed table input changing size

--- src/Utgifter.Site/src/components/ui/FocusableInput.vue
+++ src/Utgifter.Site/src/components/ui/FocusableInput.vue
@@ -14,8 +14,13 @@ const model = defineModel()
 const showInput = ref(false)
 
 const input = ref<HTMLInputElement | null>()
+const displayDiv = ref<HTMLDivElement | null>()
+const inputWidth = ref<string>('auto')
 
 function enableInput() {
+  if (displayDiv.value) {
+    inputWidth.value = `${displayDiv.value.offsetWidth}px`
+  }
   showInput.value = true
   nextTick(() => input.value?.focus())
 }
@@ -32,8 +37,9 @@ onMounted(() => {
     :type="type"
     v-if="showInput"
     v-model="model"
-    class="block w-full rounded-md border-0 bg-white pl-3 pr-3 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 text-base h-8 overflow-hidden whitespace-nowrap"
+    class="block rounded-md border-0 bg-white pl-3 pr-3 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 text-base h-8 overflow-hidden whitespace-nowrap [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
+    :style="{ width: inputWidth }"
     @blur="showInput = false"
   />
-  <div v-else class="block w-full rounded-md border-0 bg-white pl-3 pr-3 text-gray-900 text-base h-8 overflow-hidden whitespace-nowrap">{{ model }} {{ textSuffix }}</div>
+  <div ref="displayDiv" v-else class="inline-block rounded-md border-0 bg-white pl-3 pr-3 text-gray-900 text-base h-8 overflow-hidden whitespace-nowrap">{{ model }} {{ textSuffix }}</div>
 </template>
